6c739bb3ba3d8e394f3630d10e4ad5116c677c60
Eugene Park <eugenepark404@gmail.com>
2022-05-13 18:18:11 -0700

    Push to repo

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    //Sets the variables to work with the values given and calculates the interest
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate /100;
+    var year = new Date().getFullYear()+parseInt(years);
+    //Check if the principal value is positive
+    if(principal > 0){
+        document.getElementById("result").innerHTML="If you deposit "+'<mark>'+principal+'</mark>'+",\<br\>at an interest rate of "+'<mark>'+rate+'</mark>'+"%\<br\>You will receive an amount of "+'<mark>'+interest+'</mark>'+",\<br\>in the year "+'<mark>'+year+'</mark>'+"\<br\>"
+        return true;
+    }
+    //In the case that it isn't a valid number
+    alert("Enter a positive number");
+    document.getElementById("principal").focus();
 }
-        
+
+//updates the number on the rate slider
+function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+}
